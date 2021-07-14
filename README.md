dbb3c9e07167633f7e281aec0a9c8e4301bbc5af
FrankZiWANG-dev <wang.frank.zi@gmail.com>
2021-07-14 15:43:40 +0200

    commit 2

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# ajax-simple-web-service-request
+# Project
+
+## Images
+- Lightbulb image: Author: Pixabay. Link: https://www.pexels.com/photo/clear-light-bulb-placed-on-chalkboard-355952/
+- Background image: Author: Karolina Grabowska. Link: https://www.pexels.com/photo/cotton-sack-with-apricots-near-books-on-table-4397898/
--- assets/script.js
+++ assets/script.js
@@ -7,10 +7,11 @@ request.onload = function(){
         //Get JSON object from external website
         let quote= JSON.parse(this.responseText);
 
-        //places on our website where quote and author will be shown
+        //places on our website where infos will be shown
         let quoteContainer = document.getElementById("quote");
         let authorContainer = document.getElementById("author");
         let photo = document.getElementById("photo");
+        let reference = document.getElementById("reference");
 
         //function to insert quote and author into our website
         function renderQuote() {
@@ -23,12 +24,16 @@ request.onload = function(){
         //Call functions
         renderQuote();
         renderAuthor();
+        
 
-       //add img tag with src in photo div to display photo
+       //add img tag with src in photo div to display photo from external website
         if (quote.photo !== null) {
             photo.innerHTML = '<img src="'+quote.photo+'" id="photosize">';
         }
         
+        //add reference
+        reference.innerHTML = '<i> <a href="' + quote.permalink + '"> Original quote on "thatsthespir.it" </a></i> ';
+
         console.log(quote);
     }
     else {
@@ -37,3 +42,12 @@ request.onload = function(){
 };
 
 request.send();
+
+document.getElementById("reload").addEventListener("click", ()=>{
+    location.reload();
+})
+
+//add lightbulb royalty free image if no available picture
+// if (document.getElementById("photo").innerHTML == null) {
+//     document.getElementById("photo").innerHTML ='<img src="https://raw.githubusercontent.com/FrankZiWANG-dev/ajax-simple-web-service-request/main/assets/images/pexels-pixabay-355952.jpg" id="photosize">';
+// }
